50f35a77cf67edc84efe09670930a38f3e71ae5f
mahshidmn <ma.mojtabavi@gmail.com>
2024-03-19 15:00:15 +1100

    MERN-Stack Infrastructure

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import {Link} from 'react-router-dom';
+import * as userService from '../../utilities/users-service';
+function NavBar({user, setUser}) {
+
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
+
+    return (
+       <nav>
+         <div>Welcome,{user.name}</div>
+         <Link to="/orders"> Order History</Link>
+         &nbsp; |  &nbsp;
+         <Link to="/orders/new"> New Order</Link>
+         {/* <span>Welcome, { user.name }</span> */}
+         &nbsp; &nbsp;<Link to = "" onClick={handleLogOut}>Log Out</Link>
+       </nav>
+    );
+    
+
+}
+
+export default NavBar;
